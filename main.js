3a2c42cdf8b5172986bee1e35f75c128656d3789
Rickard Otvos <rickard.0413@gmail.com>
2023-07-15 14:33:19 +0200

    Added constraint on First Name

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 //By: Rickard Ötvös
 
 let menuArray = ["Start", "Load", "Options", "Exit"];
+
+function submit() {
+  var x = document.getElementById("fname").value;
+  if (x.length < 3 || hasNumber(x)) {
+    document.getElementById("constraint").style.visibility = "visible";
+  } else {
+    // Maybe it is unnecessary to remove the visibility if the user is redirected to another page
+    document.getElementById("constraint").style.visibility = "hidden";
+    document.location.href = "./index.html";
+  }
+}
+
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
